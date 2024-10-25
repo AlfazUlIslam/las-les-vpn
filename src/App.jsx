c5bb0e9757e281b3b1ce2125a0d4aac53273a02b
AlfazUlIslam <alfaz.ul.islam.mmvii@gmail.com>
2024-10-25 14:59:23 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="text-5xl">Hi</div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+import { logo } from "../../assets"
+import "./Navbar.css"
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+export default Navbar
