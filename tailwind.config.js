cdb40ff1a2a88f6b21fbb7453903d428f3e3ae94
AlfazUlIslam <alfaz.ul.islam.mmvii@gmail.com>
2024-10-25 17:27:11 +0600

    custom breakpoints added

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,6 +25,12 @@ export default {
     },
     container: {
       center: true
+    },
+    screens: {
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
     }
   },
   plugins: [],
