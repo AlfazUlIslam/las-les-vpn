a97aed2381a9c99566b326e68beb23e0f5b50956
AlfazUlIslam <alfaz.ul.islam.mmvii@gmail.com>
2024-10-25 15:33:28 +0600

    custom theme added

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,22 @@ export default {
   ],
   theme: {
     extend: {},
+    colors: {
+      lightWhite: "#F6F6F6",
+      darkWhite: "#F8F8F8",
+      lightGray: "#FFECEC",
+      darkGray: "#4F5665",
+      lightRed: "#F53855",
+      darkRed: "#F53838",
+      fadedDarkRed: "#59F53838",
+      blueishBlack: "#0B132A",
+    },
+    fontFamily: {
+      "rubik": "'Rubik', sans-serif"
+    },
+    container: {
+      center: true
+    }
   },
   plugins: [],
 }
